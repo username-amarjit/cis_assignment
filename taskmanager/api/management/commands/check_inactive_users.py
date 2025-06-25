992a5960b3ed41561a6dfb8bb3fb27de3880c88d
Amarjit <amarjityadav70173@gmail.com>
2025-06-25 06:16:06 +0530

    added command to heck inactive users

--- taskmanager/api/management/commands/check_inactive_users.py
+++ taskmanager/api/management/commands/check_inactive_users.py
@@ -7,12 +7,15 @@ class Command(BaseCommand):
     help = 'Deactivate users with 5+ in progress and pending tasks in last 7 days'
 
     def handle(self, *args, **kwargs):
+        one_week_ago = timezone.now() - timedelta(days=7)
         users = User.objects.filter(role='user', is_active=True)
 
         for user in users:
             missed_tasks = Task.objects.filter(
                 assigned_to=user,
                 status__in=['pending', 'inprogress'],
+                deadline__gte=one_week_ago,
+                deadline__lte=timezone.now()
             ).count()
             self.stdout.write(f"Checking user: {user.username}, missed tasks: {missed_tasks}")
             if missed_tasks >= 5:
